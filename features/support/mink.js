406020b6deb4b10544b80f30954e13ff5a6cbeac
Daniel Bush <dlb.id.au@gmail.com>
2016-06-26 21:15:10 +1000

    Trying to add saucelabs.

--- features/support/mink.js
+++ features/support/mink.js
@@ -23,6 +23,7 @@ var parameters = {
       accessKey: accessKey
     },
     //host: username + ':' + accessKey + '@ondemand.saucelabs.com',
+    host: username + ':' + accessKey + '@localhost',
     port: 4445
     //path: '/wd/hub'
 //"http://" + username + ":" + accessKey + "@ondemand.saucelabs.com:80/wd/hub"
