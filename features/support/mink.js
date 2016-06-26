9a85da747b841ff0a10f06a37d170f576d375fc9
Daniel Bush <dlb.id.au@gmail.com>
2016-06-26 21:02:50 +1000

    Trying to add saucelabs.

--- features/support/mink.js
+++ features/support/mink.js
@@ -9,6 +9,8 @@ This program is distributed under the terms of the BSD-2-Clause License.
 // See 'npm run test:acceptance'.
 
 var Mink = require('cucumber-mink');
+var username  = process.env.SAUCE_USERNAME;
+var accessKey = process.env.SAUCE_ACCESS_KEY;
 
 // Phantomjs - GhostDriver
 var parameters = {
@@ -17,10 +19,10 @@ var parameters = {
       browserName: 'internet explorer',
       platform: 'Windows 10',
       version: '11.103',
-      username: process.env.SAUCE_USERNAME,
-      accessKey: process.env.SAUCE_ACCESS_KEY
+      username: username,
+      accessKey: accessKey
     },
-    host: 'ondemand.saucelabs.com',
+    host: username + ':' + accessKey + '@ondemand.saucelabs.com',
     port: 80,
     path: '/wd/hub'
 //"http://" + username + ":" + accessKey + "@ondemand.saucelabs.com:80/wd/hub"
