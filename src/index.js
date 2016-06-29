8d74a13332ecb34bac120f4cc1b7a0b586fe75a5
Daniel Bush <dlb.id.au@gmail.com>
2016-06-29 19:19:31 +1000

    Trying to add saucelabs.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*
+The files in this directory are part of node-ansible-test, a javascript-based library.
+Copyright (C) 2016 Daniel Bush
+This program is distributed under the terms of the BSD-2-Clause License.
+*/
+'use strict';
+
+// This index file should live in src/.
+// 'npm build' should build it and the rest of the files in src/ to lib/.
+// package.json main should point to lib/index.js
+
---
+++ test/index_test.js
@@ -0,0 +1,37 @@
+/*
+The files in this directory are part of node-ansible-test, a javascript-based library.
+Copyright (C) 2016 Daniel Bush
+This program is distributed under the terms of the BSD-2-Clause License.
+*/
+'use strict';
+
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require("chai-as-promised");
+const wd = require('wd');
+chai.use(chaiAsPromised);
+var browser;
+var username  = process.env.SAUCE_USERNAME;
+var accessKey = process.env.SAUCE_ACCESS_KEY;
+
+describe('something', function () {
+
+  before(function () {
+    //browser = wd.promiseChainRemote('localhost');
+    browser = wd.promiseChainRemote('ondemand.saucelabs.com', 80, username, accessKey);
+    return browser.init({browserName:'chrome'});
+  });
+
+  beforeEach(function() {
+    return browser.get('https://www.google.com');
+  });
+
+  after(function () {
+    return browser.quit();
+  });
+
+  it('should retrieve the page title', function() {
+    return expect(browser.title()).to.eventually.equal('Google');
+  });
+
+});
